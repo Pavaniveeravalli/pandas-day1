93c2120213fa2a077119cd4f7bf7f04251c33e39
pavani <pavaniveeravalli502@gmail.com>
2025-11-28 14:57:21 +0530

    panda

---
+++ day1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+# data = {
+#     "Name": ["Pavani", "Ravi", "Sita"],
+#     "Age": [20, 22, 19],
+#     "City": ["Vijayawada", "Guntur", "Vizag"]
+# }
+
+# df = pd.DataFrame(data)
+
+# print(df)
+# abc=pd.read_json("./data.json")
+# print(abc)
+
+# abc1=pd.read_csv("./data.csv")
+# print(abc1)
+
+abc2=pd.read_excel("./data.xlsx")
+print(abc2)
